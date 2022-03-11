59571a5ce034b627f27a24f908dd3dfdec572d78
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2022-03-11 22:03:02 +0530

    Initial Commit

--- src/DSA/personel/Trees/TreeUse.java
+++ src/DSA/personel/Trees/TreeUse.java
@@ -1,7 +1,19 @@
 package DSA.personel.Trees;
 
+import javax.crypto.MacSpi;
+
 public class TreeUse {
     public static void main(String[] args) {
-        Node
-    }
+        Node root=new Node(4);
+        Node node1=new Node(2);
+        Node node2=new Node(3);
+        Node node3=new Node(5);
+        Node node4=new Node(6);
+        root.children(node1);
+        root.children(node2);
+        root.children(node3);
+        node2.children(node4);
+
+
+  }
 }

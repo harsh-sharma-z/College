72e49c342a7da0b8bfee27b23505f896893ef741
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2022-03-27 20:07:35 +0530

    Initial Commit

---
+++ src/DSA/hello.java
@@ -0,0 +1,8 @@
+package DSA;
+import java.util.*;
+public class hello {
+    public static void main(String[] args) {
+        String s=null;
+        System.out.println(s+" "+s.toString());
+    }
+}
